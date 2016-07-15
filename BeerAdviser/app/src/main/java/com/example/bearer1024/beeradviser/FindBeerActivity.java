0020b8d95cffb693e377f8db0962426ac3fa8341
bearer1024 <bearer1024@gmail.com>
2016-07-15 17:43:52 +0100

    add customer java class in BeerAdviser app

--- BeerAdviser/app/src/main/java/com/example/bearer1024/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/example/bearer1024/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.example.bearer1024.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view){
+        //get a reference to the textView
+        TextView brands =  (TextView)findViewById(R.id.brands);
+        brands.setText("gootle of geer");
+
+        //get a reference to the spinner
+        Spinner color = (Spinner)findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Display the selected item
+        brands.setText(beerType);
     }
 }
