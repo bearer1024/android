6525a89a60735963fdb70afbe26e12d01c64f0a3
bearer1024 <bearer1024@gmail.com>
2016-08-19 19:00:52 +0100

    added android google-map official demo to the repo

--- leicesterCampusPhpAndroid/app/src/main/java/com/leicesterCampus/registrationandlogin/MapsActivity.java
+++ leicesterCampusPhpAndroid/app/src/main/java/com/leicesterCampus/registrationandlogin/MapsActivity.java
@@ -1,7 +1,15 @@
 package com.leicesterCampus.registrationandlogin;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,8 +18,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements
+        OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
     private GoogleMap mMap;
 
     @Override
@@ -37,10 +49,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMylocation();
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void enableMylocation(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED){
+            //permission to access the location is misssing
+            PermissionUtils.requestPermission(this,LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,true);
+        }else if(mMap!=null){
+            //Access to the location has been granted to the map
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResult){
+        if(requestCode != LOCATION_PERMISSION_REQUEST_CODE){
+            return ;
+        }
+
+        if(PermissionUtils.isPermissionGranted(permissions,grantResult,Manifest.permission.ACCESS_FINE_LOCATION)){
+            //Enable the my location layer if the permission has been granted.
+            enableMylocation();
+        }else{
+            //Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments(){
+        super.onResumeFragments();
+        if(mPermissionDenied){
+            //Permission was not granted, display error dialog
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /*
+    display a dialog with error message explaining that the location permission is missing
+     */
+    private void showMissingPermissionError(){
+        PermissionUtils.PermissionDeniedDialog.newInstance(true).
+                show(getSupportFragmentManager(),"dialog");
     }
 }
